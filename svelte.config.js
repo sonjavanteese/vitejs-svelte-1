0d40a4332b12e4bea4e510f8c163220705e61b9d
sonjavanteese <35209042+sonjavanteese@users.noreply.github.com>
2023-02-02 15:01:44 +0100

    setup app

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,20 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
+import preprocess from "svelte-preprocess";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
+  extensions: [".svelte", ...mdsvexConfig.extensions],
+
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+  preprocess: [
+    vitePreprocess(),
+    preprocess({
+      scss: {
+        prependData: '@use "src/variables.scss" as *;',
+      },
+    }),
+    mdsvex(mdsvexConfig),
+  ],
+};
